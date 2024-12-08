d348c86089acb88bd6a969a51758a6dbf1903b43
adamp <adamp@DESKTOP-IHA4B1D>
2024-12-08 20:32:33 +0000

    test commit

--- src/advprogtaba/AdvProgTABA.java
+++ src/advprogtaba/AdvProgTABA.java
@@ -14,7 +14,7 @@ public class AdvProgTABA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("dsad");
+        System.out.print("dsasd");
     }
     
 }
